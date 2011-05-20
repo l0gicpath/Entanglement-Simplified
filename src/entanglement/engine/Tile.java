c63adc926a6f95d2fd77cb1c763d849308581b10
Hussein Fawzy <hussein_fawzy_1@hotmail.com>
2011-05-20 17:15:00 +0200

    clockWise added

--- src/entanglement/engine/Tile.java
+++ src/entanglement/engine/Tile.java
@@ -21,4 +21,17 @@ public class Tile {
 	public void setTileConf(int[] tileConf) {
 		this.tileConf = tileConf;
 	}
+	
+	public void rotateClockWise(){
+		int[] tmpConf = new int[tileConf.length];
+		for (int i = 0;i < tileConf.length;i++)
+		{
+			if (i < tileConf.length - 2)
+				tmpConf[i + Config.inst().openingsPerSide] = tileConf[i];
+			else
+				tmpConf[i - tileConf.length - 2] = tileConf[i];
+		}
+		
+		tileConf = tmpConf;
+	}
 }
